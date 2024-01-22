c047989c76e801f9d5c6e4c822e919ba05912347
Alexandra <alexa_luz@mail.ru>
2024-01-22 12:02:44 +0300

    3 решение

--- Program.cs
+++ Program.cs
@@ -42,26 +42,57 @@
 //Задайте массив заполненный случайными трёхзначными числами.
 //Напишите программу, которая покажет количество чётных чисел в массиве.
 
+//using System;
+//class Program
+//{
+//static void Main()
+//{
+//int[] numbers = new int[10];
+//Random random = new Random();
+//int evenCount = 0;
+
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//numbers[i] = random.Next(100, 1000);
+//Console.Write(numbers[i] + " ");
+
+//if (numbers[i] % 2 == 0)
+//{
+//evenCount++;
+//}
+//}
+
+//Console.WriteLine($"\nКоличество четных чисел в массиве: {evenCount}");
+//}
+//}
+
+
+//Напишите программу, которая перевернёт одномерный массив
+//(первый элемент станет последним, второй – предпоследним и т.д.)
+
+
 using System;
 class Program
 {
 static void Main()
 {
-int[] numbers = new int[10];
-Random random = new Random();
-int evenCount = 0;
-
-for (int i = 0; i < numbers.Length; i++)
+int[] numbers = {5, 7, 3, 1, 6, 8};
+int temp;
+Console.Write("Исходный массив: ");
+foreach (int number in numbers)
 {
-numbers[i] = random.Next(100, 1000);
-Console.Write(numbers[i] + " ");
-
-if (numbers[i] % 2 == 0)
+Console.Write(number + " ");
+}
+for (int i = 0; i < numbers.Length / 2; i++)
 {
-evenCount++;
+temp = numbers[i];
+numbers[i] = numbers[numbers.Length - 1 - i];
+numbers[numbers.Length - 1 - i] = temp;
 }
+Console.Write("\nПеревернутый массив: ");
+foreach (int number in numbers)
+{
+Console.Write(number + " ");
 }
-
-Console.WriteLine($"\nКоличество четных чисел в массиве: {evenCount}");
 }
 }
